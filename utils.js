4cf633a734059121e630a28eec82127f96faa6fa
angro <mail@angro.org>
2021-06-15 20:09:46 +0300

    add favicon eHTML eJSON

--- utils.js
+++ utils.js
@@ -1,6 +1,7 @@
 
 const zlib = require('zlib')
 const { STATUS_CODES } = require('http')
+const { PassThrough } = require('stream')
 
 // globalThis.errorAcc = []
 errors = (code, msg, error) => {
@@ -14,7 +15,7 @@ errors = (code, msg, error) => {
 module.exports.errors = errors
 
 module.exports.contentType = str => {
-    let t, c
+    let t = null, c = null
 
     if(str) {
         const _a = str.replace(';', ' ').split(' ').filter(Boolean)
@@ -23,7 +24,7 @@ module.exports.contentType = str => {
     }
 
     switch (c) { // TODO: more charsets
-        case undefined:
+        case null:
             c = 'utf8'
             break
         case 'utf-8':
@@ -42,8 +43,8 @@ module.exports.contentType = str => {
 module.exports.urlParser = url => {
     const _i = (_p = url.indexOf('?')) > 0 ? _p : url.length
     const urn = (_u = url.substring(0, _i).split('/').filter(Boolean)).length ? _u : ['/']
-    const urlParam = (query = url.substring(_i + 1)).length
-        ? decodeURIComponent(query).split('&').filter(Boolean)
+    const urlParam = (_q = url.substring(_i + 1)).length
+        ? decodeURIComponent(_q).split('&').filter(Boolean)
         .reduce((a, c) => {
             const _z = c.split('=')
             a[_z[0]] = _z[1]
@@ -54,12 +55,12 @@ module.exports.urlParser = url => {
     return { urn, urlParam }
 }
 
-module.exports.acceptEncoding = str => {
+module.exports.getEncoding = str => {
     // https://datatracker.ietf.org/doc/html/rfc7231#section-5.3.4
     if(!str) return null
 
     let _a = str
-        .split(',')
+        .split(',') // TODO: , => ' '
         .filter(Boolean)
         .reduce((a, c) => {
             a.push(c.split(';').map(v => {
@@ -69,8 +70,9 @@ module.exports.acceptEncoding = str => {
         }, [])
 
     // "identity;q=0" or "*;q=0"
-    if(_a.length == 1 & _a[0][0] == '*' & _a[0][1] == '0') return null
-    if(_a.length == 1 & _a[0][0] == 'identity' & _a[0][1] == '0') return null
+    if(_a.length == 1 && _a[0][0] == 'identity') return null
+    if(_a.length == 1 && (_a[0][0] == '*' && _a[0][1] == '0')) return null
+    if(_a.length == 1 && (_a[0][0] == 'identity' && _a[0][1] == '0')) return null
 
     _a = _a
         .map(v => { if (['gzip', 'deflate', 'br', '*'].includes(v[0])) return v })
@@ -93,4 +95,68 @@ module.exports.contentEncoders = {
     gzip: zlib.createGzip,
     deflate: zlib.createDeflate,
     br: zlib.createBrotliCompress,
+}
+
+
+module.exports.favicon = async reqData => {
+    const ico = `iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA
+    7DAcdvqGQAAAHDSURBVDhPY2TAAtZdb2Nk+PfHCcg0AeL/QHyWgYllX5BmFYiNApigNBysu9okDdTc9P//P8Z7b09duPv25Js//35xAcXqwXJoAMUFa
+    683szH++1/9+uv97k8/Xq4HCrkA8U8gli+12/YSaEAzIxNDc6Bm3S+QehBAcQFQcziQmv/5x0tuIK0NFmRgWALSDGEyzv7/jyEawoYAFigNAxJB2nUP
+    QIxF5/I02Vi4XrIxcxr2HWFL/Pvvl8e9t8eylIQtRcEqoQDdAObuQ16qQHruu+9PHsgK6LMDQ80IGI7zQJL/GRjngtSA2DCAHogsPKxCb4C00e+/P2K
+    ffbx2/9//v/Z///1+DhT7z8rM8RhI4zVghTifWgSQzgXifz/+fN4aqtN0CMguBeJeOQEDWyC9HIjhAMUAoP/vAKnvysIWv4EBpgF0fg1IXIPLbBlQ7D
+    aQ+Reo5i5IDAawJ6SrTXZAyoGDhfcfw///LD/+fgFF5VGgZpBriAefv3yq+ATEUC5WgJESSQUDbwDWQOw+5A1KPJOYGVmUgUoY//7/fRNIF5TabT0PV
+    QIHOFzwH2gAw+G///8sAGqeD2QfA4oZgqVQAAMDANfApeNG0BOMAAAAAElFTkSuQmCC`
+
+    return { code: 200, headers: { 'Content-Type': 'image/x-icon' }, encode: false,
+        resStream: new PassThrough().end(Buffer.from(ico, 'base64'))
+    }
+}
+
+module.exports.eHTML = async code => {
+    const body = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>${code}</title>
+    </head>
+    <body style="background-color: #161b22">
+        <div style="
+            display: flex;
+            justify-content: center;
+            font-size: xx-large;
+            font-weight: bold;
+            color: olivedrab;
+            "
+        >
+            ${STATUS_CODES[code]}
+        </div>
+    </body>
+    </html>
+    `
+
+    return { code: code, headers: { 'Content-Type': 'text/html' },
+        resStream: new PassThrough().end(Buffer.from(body, 'utf8'))
+    }
+}
+
+module.exports.eJSON = async data => {
+
+    const body = typeof data !== 'number' ? data : {
+        errors: [
+            {
+                message: STATUS_CODES[data],
+                originalError: {
+                    name: data,
+                    message: STATUS_CODES[data]
+                }
+            }
+        ]
+    }
+
+    const code = typeof data !== 'number' ? 500 : data
+
+    return { code, headers: { 'Content-Type': 'application/json' },
+        resStream: new PassThrough().end(Buffer.from(JSON.stringify(body), 'utf8'))
+    }
 }
